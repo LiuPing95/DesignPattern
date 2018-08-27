4eba53f9e468b78a210099dc87c84c35d10bfcfe
LiuPing <465638135@qq.com>
2018-08-28 00:04:12 +0800

    create package of every pattern, and an gitignore file

--- src/creation/factoryMethod/HumanFactory.java
+++ src/creation/factoryMethod/HumanFactory.java
@@ -16,7 +16,8 @@ public class HumanFactory extends AbstractHumanFactory {
      * 简单工厂方法模式：如果模块只需要一个工厂，那么不需要定义父类或接口，把方法写成静态的
      * 多个工厂方法模式：在复杂的业务场景中，每一个工厂负责某一类对象的创建。
      * */
-    public <T extends Human> T createHuman(Class<T> c) {
+    @SuppressWarnings("unchecked")
+	public <T extends Human> T createHuman(Class<T> c) {
         T t = null;
         if (c != null) {
             try {
